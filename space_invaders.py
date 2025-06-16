739be34faa2852a9f6d018dff0012290ba094d3f
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-06-16 15:36:54 +0800

    added bg and fps

--- space_invaders.py
+++ space_invaders.py
@@ -1,17 +1,38 @@
 import pygame 
 from pygame.locals import *
 
+# Set Frames per second
+clock = pygame.time.Clock()
+fps = 60
+
+#Initialize the interface
 screen_width = 600
 screen_height = 800
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Space Invaders")
 
+
+#Load background image
+bg = pygame.image.load("img/bg.png")
+
+def draw_bg(): 
+    screen.blit(bg, (0, 0))
+
+
+#Loop for the game to run
 run = True 
 while run:
+
+    clock.tick(fps)
+
+    draw_bg()
+
     for event in pygame.event.get():
         if event.type == QUIT:
             run = False
+
+    pygame.display.update()
 
 pygame.quit()
     
