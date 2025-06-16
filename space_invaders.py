0e11a76186b6265a9c661a473c1cd305f046f563
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-06-16 15:22:38 +0800

    space invaders initial windows

---
+++ space_invaders.py
@@ -0,0 +1,18 @@
+import pygame 
+from pygame.locals import *
+
+screen_width = 600
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Space Invaders")
+
+run = True 
+while run:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            run = False
+
+pygame.quit()
+    
+
